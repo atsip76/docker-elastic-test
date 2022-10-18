ae7979209ecf62e815885d25fd9d2ffc9ee93139
Anatoli Afanasiev <aan@usedesk.ru>
2022-10-18 14:00:58 +0500

    modified:   deployStack.sh 	modified:   docker-compose.yml 	modified:   filebeat-docker-compose.yml

--- deployStack.sh
+++ deployStack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export ELASTIC_VERSION=7.9.1
+export ELASTIC_VERSION=7.17.6
 export ELASTICSEARCH_USERNAME=elastic
-export ELASTICSEARCH_PASSWORD=changeme
-export ELASTICSEARCH_HOST=node1
-export INITIAL_MASTER_NODES=node1
+export ELASTICSEARCH_PASSWORD=usedesk
+export ELASTICSEARCH_HOST=elastic-master01
+export INITIAL_MASTER_NODES=elastic-master01
 
 docker network create --driver overlay --attachable elastic
 docker stack deploy --compose-file docker-compose.yml elastic
